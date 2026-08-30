8df21af57b973c9eaed8952017f761a09e478a40
Irfan Uruchi <dex018065@gmail.com>
2026-08-30 14:07:05 +0200

    Revert "Activate IULinux premium window decoration"
    This reverts commit 654cafab80bbabb0324ecc74485b563b8a0cc62d.

--- tests/test-branding.sh
+++ tests/test-branding.sh
@@ -34,7 +34,7 @@ grep -q '"Id": "com.iulinux.desktop"' \
 [[ -f "${LAF}/contents/layouts/org.kde.plasma.desktop-layout.js" ]] ||
     fail "IULinux Plasma desktop layout missing"
 
-grep -qx 'ColorScheme=IULinuxDark' \
+grep -qx 'ColorScheme=BreezeDark' \
     "${LAF}/contents/defaults" ||
     fail "IULinux color scheme policy missing"
 
